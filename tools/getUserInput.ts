8f08f33750544d6633af7ff6357525c4b814c72f
Jason Wang <wanghung07@gmail.com>
2023-09-20 14:22:51 +0200

    fix enters blank line crashes the process

--- tools/getUserInput.ts
+++ tools/getUserInput.ts
@@ -1,18 +1,21 @@
 import * as readline from "readline"
-import { Position, Stage, isValidType } from "../tools/typeChecking"
+import { Position, Stage } from "../tools/typeChecking"
 
 export async function getUserInput(stage: Stage): Promise<Position> {
     let questionText: string
 
     switch (stage) {
         case "placing":
-            questionText = "Place a cheese: "
+            questionText = "Place a cheese:"
             break
         case "selecting":
-            questionText = "Select a cheese to move: "
+            questionText = "Select a cheese to move:"
+            break
+        case "moving":
+            questionText = "Select a spot that you want to move to: "
             break
         default:
-            questionText = "Select a spot to place your cheese: "
+            questionText = "Invalid stage"
     }
 
     const rl = readline.createInterface({
@@ -20,18 +23,19 @@ export async function getUserInput(stage: Stage): Promise<Position> {
         output: process.stdout,
     })
 
-    try {
-        const input = await new Promise<string>((resolve) => {
+    let input: string = ""
+
+    while (input.trim() === "") {
+        input = await new Promise<string>((resolve) => {
             rl.question(questionText, resolve)
         })
 
-        if (isValidType(input)) {
-            return input as Position
-        } else {
-            console.log("Invalid position, please try again.")
-            return getUserInput(stage) // Recursively call the function on invalid input
+        if (input.trim() === "") {
+            console.log("Invalid input. Please provide a non-empty value.")
         }
-    } finally {
-        rl.close() // Ensure readline is closed after input
     }
+
+    rl.close()
+
+    return input.trim() as Position
 }
